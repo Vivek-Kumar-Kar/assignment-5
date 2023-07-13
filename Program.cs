b3ce3e7d6abfe8a0588bed26ea2f66ebd92e0892
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-07-13 15:12:03 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string validUsername = "vivek";
+            string validPassword = "123456789";
+            int attempts = 0;
+
+            while (attempts < 3)
+            {
+                Console.Write("Enter username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+
+                if (username == validUsername && password == validPassword)
+                {
+                    Console.WriteLine("Login successful");
+                    break;
+                }
+                else if (username == validUsername && password != validPassword)
+                {
+                    Console.WriteLine("Invalid password.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username.");
+                }
+
+                attempts++;
+            }
+
+            if (attempts == 3)
+            {
+                Console.WriteLine("Login failed.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
